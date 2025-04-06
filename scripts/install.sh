d6582f26035c90b26215a38cf55961bb9241c0a1
Hunter McKay <huntmckay@gmail.com>
2025-04-05 21:10:47 -0400

    update installs

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,8 +3,13 @@ vim
 tree
 bpython
 stow
+<<<<<<< Updated upstream
 tmux
 
 # deno2
 curl -fsSL https://deno.land/install.sh | sh
 deno --version
+=======
+tealdeer
+tldr --update
+>>>>>>> Stashed changes
