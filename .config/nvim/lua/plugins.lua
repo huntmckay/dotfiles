a1cdbbf07c089175781ed440846e6b216c7623a9
Hunter McKay <huntmckay@gmail.com>
2025-04-07 18:20:16 -0400

    adds comment plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -25,4 +25,28 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = false, -- we configure it manually in ui.lua
   },
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup({
+        toggler = {
+          line = "<leader>c",   -- Line comment toggle
+          block = "<leader>C",  -- Block comment toggle
+        },
+        opleader = {
+          line = "<leader>c",   -- Visual mode line toggle
+          block = "<leader>C",  -- Visual mode block toggle
+        },
+        mappings = {
+          basic = true,
+          extra = false,
+        },
+      })
+      -- Custom remaps for "extra" comment features not sure if work
+      vim.keymap.set("n", "<leader>co", api.comment_linewise_above, { desc = "Comment above", noremap = true })
+      vim.keymap.set("n", "<leader>ca", api.comment_linewise_current, { desc = "Comment end of line", noremap = true })
+    end,
+  },
+  { "lukas-reineke/indent-blankline.nvim" },
+  { "echasnovski/mini.surround" },
 }
