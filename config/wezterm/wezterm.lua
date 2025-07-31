47b44dc326e9ad42d1d648dc6c650e664d1a4281
hmckay <huntmckay@gmail.com>
2025-07-31 12:04:42 -0400

    wezterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,7 +3,6 @@ local config = wezterm.config_builder()
 
 local theme = wezterm.plugin.require('https://github.com/neapsix/wezterm').main
 
-
 local function get_appearance()
   if wezterm.gui then
     return wezterm.gui:get_appearance()
@@ -17,21 +16,15 @@ local function scheme_for_appearance(appearance)
   end
 end
 
--- Apply dynamic theme based on appearance
 config.color_scheme = scheme_for_appearance(get_appearance())
-
--- General settings
 config.enable_wayland = false
 config.window_background_opacity = 1.0
 config.text_background_opacity = 0.3
 config.font = wezterm.font 'Mononoki Nerd Font Mono'
---I want a wezterm command to change my bg
 -- config.window_background_image = wezterm.home_dir .. '/Pictures/slurry.jpg'
-config.use_fancy_tab_bar = true
-
-config.use_fancy_tab_bar = false         -- disables the tab bar rendering
-config.hide_tab_bar_if_only_one_tab = true -- hides it entirely when only one tab is open
-config.window_decorations = "RESIZE"     -- disables title bar, but keeps resize handles
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
 
 wezterm.log_info("wezterm config loaded")
 
